a94b93e9e84131043c6d32e37089c8f3e6b78b60
Andrei Yangabishev <waffleboot@gmail.com>
2021-04-14 21:59:19 +0300

    refactor day2

--- day2.sql
+++ day2.sql
@@ -1,45 +1,50 @@
 
 -- Largest Organization
-WITH RECURSIVE t AS
-(
-    SELECT name,
+WITH RECURSIVE t AS (
+    SELECT id as root,
            id
       FROM organization
      WHERE parent IS NULL
      UNION ALL
-    SELECT t.name,
-           o.id
-      FROM organization o, t
-     WHERE o.parent = t.id
+    SELECT t.root,
+           ch.id
+      FROM organization ch
+     INNER JOIN t ON ch.parent = t.id
+), r AS (
+SELECT root,
+       count(*) cnt
+  FROM t
+ GROUP BY root
+),
+q AS (
+SELECT r.*,
+       rank() OVER (ORDER BY cnt DESC) rnk
+  FROM r
 )
 SELECT name,
-       count(*) AS cnt
-  FROM t
- GROUP BY 1
- ORDER BY 2 DESC
- LIMIT 1;
+       cnt
+  FROM q
+ INNER JOIN organization o ON o.id = q.root
+ WHERE rnk = 1;
 
 -- Materialized path
 ALTER TABLE organization ADD IF NOT EXISTS path INTEGER[];
 WITH RECURSIVE t AS
 (
     SELECT id,
-           parent,
-           0 as lvl
+           array[]::integer[] path
       FROM organization
-     WHERE parent IS NOT NULL
+     WHERE parent IS NULL
      UNION ALL
-    SELECT t.id,
-           p.parent,
-           t.lvl + 1
-      FROM t, organization p
-     WHERE t.parent = p.id
-       AND p.parent IS NOT NULL
+    SELECT ch.id,
+           t.path || t.id
+      FROM t
+     INNER JOIN organization ch ON ch.parent = t.id
 )
 UPDATE organization o
-   SET path = q.path
-  FROM (SELECT id, array_agg(parent ORDER BY lvl DESC) path FROM t GROUP BY id) q
- WHERE o.id = q.id;
+   SET path = t.path
+  FROM t
+ WHERE t.id = o.id;
 
 -- Use materialized path
 SELECT * FROM organization WHERE 2 = ANY (path);
