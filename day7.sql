392a4f9bc87a73286b9f54b86ab6018a8c3cbc69
Andrei Yangabishev <waffleboot@gmail.com>
2021-04-23 01:10:04 +0300

    too complex case

--- day7.sql
+++ day7.sql
@@ -15,106 +15,28 @@ insert into test_patient values ($$
     "system":"emal"
 },{
     "use":"mobile",
-    "value":"+7-981-123-12-01",
+    "value":"   +  7-981-123-12-01",
     "system":"phone"
 }]
 }$$);
 -- select jsonb_pretty(resource) from test_patient;
 
--- для каждого ресурса вытащить в виде json массива все пути до telecom[*].value, оставив только telecom mobile +7
--- дальше каждый ресурс рекурсивно обновлять через jsonb_set, строя пути до value, на каждом шаге рекурсии json массив уменьшается
--- для обновления всегда берется первый элемент и рекурсия пока массив не иссякнет
--- t3 := resource | json [ pth ] where pth := { "pth" : ["telecom",2,"value"] , "value" : "+7..." }
-WITH RECURSIVE t3 AS (
 
-    SELECT tp.resource,
-           jp.pths
-      FROM test_patient tp,
-   lateral /*jp*/ (
-
-        WITH RECURSIVE keys AS
-        (
-            SELECT '[]'::jsonb pth,
-                   tp.resource,
-                   null::bool telecom_mobile,
-                   null::bool upd
-             UNION ALL
-            SELECT kv.*
-              FROM keys,
-           lateral /*kv*/ (
-                    SELECT keys.pth||to_jsonb(t.key),
-                           t.value,
-                           t.key = 'telecom',
-                           keys.telecom_mobile AND t.key = 'value' AND starts_with(t.value #>> '{}', '+7') upd
-                      FROM jsonb_each(resource) t
-                     WHERE jsonb_typeof(resource) = 'object'
-                     UNION ALL 
-                    SELECT keys.pth||to_jsonb(t.pos-1),
-                           t.element,
-                           keys.telecom_mobile AND t.element @> '{"use":"mobile","system":"phone"}',
-                           null
-                      FROM jsonb_array_elements(resource) WITH ordinality t(element,pos)
-                     WHERE jsonb_typeof(resource) = 'array'
-            ) kv
-        )
-        -- filter by upd and aggregate to [ { "pth" : ["telecom",2,"value"] , "value" : "+7..." } ]
-        SELECT jsonb_agg(jsonb_build_object('pth',pth,'value',resource)) pths
-          FROM keys
-         WHERE upd
-
-      ) jp
-    
-    UNION ALL
-
-    -- get first element from json array, use it pth as path, replace value in resource and run it again on rest elements
-    SELECT jsonb_set(
-               t3.resource,
-               (SELECT array_agg(t) FROM jsonb_array_elements_text(t3.pths -> 0 -> 'pth') t), -- text[]
-               to_jsonb(overlay((t3.pths -> 0 ->> 'value') placing '8' from 1 for 2))
-           ),
-           t3.pths - 0
-      FROM t3
-     WHERE t3.pths != '[]'
-)
-SELECT jsonb_pretty(resource)
-  FROM t3
- WHERE t3.pths = '[]';
-
-
-
-
-/*WITH RECURSIVE keys AS
-(
-    SELECT '[]'::jsonb pth,
-           resource,
-           null::bool telecom,
-           null::bool mobile,
-           null::bool upd
-      FROM test_patient
-     UNION ALL
-    SELECT nxt.*
-      FROM keys,
-      lateral (
-            SELECT keys.pth||to_jsonb(t.key),
-                   t.value,
-                   t.key = 'telecom',
-                   keys.mobile,
-                --    keys.mobile AND t.key = 'value'
-                   keys.mobile AND t.key = 'value' AND starts_with(t.value #>> '{}', '+7')
-              FROM jsonb_each(resource) t
-             WHERE jsonb_typeof(resource) = 'object'
-             UNION ALL 
-            SELECT keys.pth||to_jsonb(t.pos-1),
-                   t.value,
-                   keys.telecom,
-                --    keys.telecom,
-                   keys.telecom AND t.value @> '{"use":"mobile","system":"phone"}',
-                   null
-              FROM jsonb_array_elements(resource) with ordinality t(value,pos)
-             WHERE jsonb_typeof(resource) = 'array'
-     ) nxt
-)
-select jsonb_agg(pth) pth
-    --  , resource
-  from keys
- where upd;*/
+SELECT resource || (SELECT jsonb_build_object('telecom', jsonb_agg(
+       CASE
+       WHEN t.value @?
+       $$
+           $ ? (
+               @.use == "mobile" && 
+               @.system == "phone" && 
+               @.value like_regex "^[[:space:]]*[+][[:space:]]*7")
+       $$
+       THEN t.value || jsonb_build_object(
+           'value',
+           regexp_replace(
+               t.value ->> 'value',
+               '^[[:space:]]*[+][[:space:]]*7',
+               '8'))
+       ELSE t.value
+       END)) FROM jsonb_array_elements(resource #> '{telecom}') t)
+  FROM test_patient;
