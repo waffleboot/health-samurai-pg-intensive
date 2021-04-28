21088b7513b5506634e223101ef44bdd3b71db13
Andrei Yangabishev <waffleboot@gmail.com>
2021-04-29 00:42:53 +0300

    fix day 9

--- day9.sql
+++ day9.sql
@@ -1,9 +1,12 @@
 
 CREATE FUNCTION encounter_period (resource jsonb) RETURNS daterange AS $$
+DECLARE
+  s date := (resource #>> '{period,start}');
+  e date := (resource #>> '{period,end}');
 BEGIN
   RETURN daterange(
-      (resource #>> '{period,start}')::date,
-      (resource #>> '{period,end}')::date,
+      (date_trunc('month', s))::date,
+      (date_trunc('month', e))::date,
       '[]');
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
@@ -17,7 +20,8 @@ SELECT count(*)
 SELECT jsonb_pretty(resource -> 'period'),
        encounter_period(resource)
   FROM encounter 
- WHERE encounter_period(resource) @> '2020-01-01'::date;
+ WHERE encounter_period(resource) @> '2020-01-01'::date
+ LIMIT 1;
 
 explain (analyze, costs off, timing off)
 SELECT jsonb_pretty(resource -> 'period'),
